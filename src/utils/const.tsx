50b71dd75a4aca177492866b64b735110c323a42
Igor Makarev <igor.makarev@nizhpharm.ru>
2024-10-08 16:48:37 +0300

    tests

--- src/utils/const.tsx
+++ src/utils/const.tsx
@@ -1,3 +1,4 @@
 export const API_BASE = 'https://norma.nomoreparties.space/api';
 export const WS_SOCKET = 'wss://norma.nomoreparties.space/orders/all';
 export const WS_SOCKET_USER = 'wss://norma.nomoreparties.space/orders';
+export const testUrl = 'http://localhost:3000';
